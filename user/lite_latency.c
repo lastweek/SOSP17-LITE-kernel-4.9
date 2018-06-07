57f6684c946a4d2b428b0e3f22a81d4482d326df
Yizhou Shan <yizhous@vmware.com>
2018-06-07 09:42:06 -0700

    Update latency benchmark

--- user/lite_latency.c
+++ user/lite_latency.c
@@ -19,7 +19,6 @@
 #include <malloc.h>
 #include "lite-lib.h"
 
-int run_times = 10;
 static int remote_node;
 static unsigned int pg_size;
 
@@ -39,7 +38,7 @@ static inline void die(const char * str, ...)
  * How many runs each size will run.
  * Amortize random variation.
  */
-#define NR_TESTS_PER_SIZE	(1000)
+#define NR_TESTS_PER_SIZE	(1000000)
 #define NSEC_PER_SEC	(1000*1000*1000)
 
 static inline long timespec_diff_ns(struct timespec end, struct timespec start)
@@ -69,7 +68,7 @@ static void rdma_write_read()
 						      MAX_BUF_SIZE, 0, password);
         printf("Finish remote mem alloc. Key: %#lx %ld\n", test_key, test_key);
 
-	printf(" Test RDMA Write\n");
+	printf(" Test RDMA Write (avg of # %d run)\n", NR_TESTS_PER_SIZE);
 	for (i = 0; i < ARRAY_SIZE(testsize); i++) {
 		struct timespec start, end;
 		long diff_ns;
@@ -81,8 +80,24 @@ static void rdma_write_read()
 		clock_gettime(CLOCK_MONOTONIC, &end);
 		diff_ns = timespec_diff_ns(end, start);
 
-		printf("    size = %#10x avg_time = %15ld ns\n",
-			testsize[i], diff_ns/NR_TESTS_PER_SIZE);
+		printf("    size = %#10x (%10d) avg_time = %15ld ns\n",
+			testsize[i], testsize[i], diff_ns/NR_TESTS_PER_SIZE);
+	}
+
+	printf(" Test RDMA Read (avg of # %d run)\n", NR_TESTS_PER_SIZE);
+	for (i = 0; i < ARRAY_SIZE(testsize); i++) {
+		struct timespec start, end;
+		long diff_ns;
+
+		clock_gettime(CLOCK_MONOTONIC, &start);
+		for (j = 0; j < NR_TESTS_PER_SIZE; j++) {
+			userspace_liteapi_rdma_read(test_key, buf, testsize[i], 0, password);
+		}
+		clock_gettime(CLOCK_MONOTONIC, &end);
+		diff_ns = timespec_diff_ns(end, start);
+
+		printf("    size = %#10x (%10d) avg_time = %15ld ns\n",
+			testsize[i], testsize[i], diff_ns/NR_TESTS_PER_SIZE);
 	}
 }
 
