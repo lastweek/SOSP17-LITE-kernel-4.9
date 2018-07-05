d31d1a613d7e2af5f2631b29bcc676a8c5f6227e
Yizhou Shan <yizhous@vmware.com>
2018-07-05 10:44:10 -0700

    Update lite_latency bench

--- user/lite_latency.c
+++ user/lite_latency.c
@@ -18,23 +18,34 @@
 #include <stdarg.h>
 #include <pthread.h>
 #include <time.h>
-#include <sched.h>
 #include <sys/mman.h>
 #include <sys/time.h>
 #include <sys/syscall.h>
+#include <stdbool.h>
 #include <malloc.h>
+#include <getopt.h>
+#include <sched.h>
 #include "lite-lib.h"
 
-static int remote_node;
-static unsigned int pg_size;
+struct thread_info {
+	int tid;
+	int remote_nid;
+};
 
-static inline void die(const char * str, ...)
+static int nr_threads = 1;
+static int remote_node = -1;
+static double *per_rps;
+static pthread_barrier_t thread_barrier;
+static unsigned int pg_size;
+static uint64_t test_key;
+
+static inline void die(const char *str, ...)
 {
-        va_list args;
-        va_start(args, str);
-        vfprintf(stderr, str, args);
-        fputc('\n', stderr);
-        exit(1);
+	va_list args;
+	va_start(args, str);
+	vfprintf(stderr, str, args);
+	fputc('\n', stderr);
+	exit(1);
 }
 
 #define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
@@ -56,15 +67,46 @@ static inline long timespec_diff_ns(struct timespec end, struct timespec start)
 	return e - s;
 }
 
-int testsize[] = {8,64,512,1024,1024*2,1024*4,1024*8};
+static int bind_thread(int cpu_id)
+{
+	cpu_set_t cpu_set;
 
-static void rdma_write_read()
+	CPU_ZERO(&cpu_set);
+	CPU_SET(cpu_id, &cpu_set);
+
+	return sched_setaffinity(0, sizeof(cpu_set), &cpu_set);
+}
+
+int testsize[] = { 8, 64, 512, 1024, 1024 * 2, 1024 * 4, 1024 * 8 };
+
+static void print_rps(int size_idx, const char *who)
+{
+	int i;
+	double total = 0;
+
+	for (i = 0; i < nr_threads; i++)
+		total += per_rps[i];
+
+	printf("\033[31m nr_threads: %d, each thread performed #%d %s, avg throughput: %10lf size=%#10x (%10d)\033[0m\n",
+		nr_threads, NR_TESTS_PER_SIZE, who, total/nr_threads,
+		testsize[size_idx], testsize[size_idx]);
+}
+
+static void *rdma_write_read(void *_info)
 {
 	int i, j;
-	uint64_t test_key;
-	int password=100;
+	int password = 100;
 	char *buf;
 	int *poll;
+	struct thread_info *info = _info;
+
+	/*
+	 * XXX
+	 * Hard-coded distribution
+	 */
+	bind_thread(55 - info->tid);
+	printf("  thread[%d] running on CPU%2d\n",
+		info->tid, sched_getcpu());
 
 	buf = aligned_alloc(pg_size, MAX_BUF_SIZE);
 	if (!buf)
@@ -75,31 +117,21 @@ static void rdma_write_read()
 
 	memset(buf, 'A', 1024 * 64);
 
-	test_key = userspace_liteapi_alloc_remote_mem(remote_node,
-						      MAX_BUF_SIZE, 0, password);
-        printf("Finish remote mem alloc. Key: %#lx %ld\n", test_key, test_key);
+	/*
+	 * Let one thread do the allocation
+	 * Rest of the team reuse the same buffer.
+	 */
+	if (pthread_barrier_wait(&thread_barrier) ==
+	    PTHREAD_BARRIER_SERIAL_THREAD) {
+		test_key = userspace_liteapi_alloc_remote_mem(remote_node,
+			      MAX_BUF_SIZE, 0, password);
+		printf("Finish remote mem alloc. Key: %#lx %ld\n",
+			test_key, test_key);
 
-	printf(" \033[31mTest RDMA Write (avg of # %d run)\033[0m\n", NR_TESTS_PER_SIZE);
-	for (i = 0; i < ARRAY_SIZE(testsize); i++) {
-		struct timespec start, end;
-		long diff_ns;
-		double rps;
-
-		clock_gettime(CLOCK_MONOTONIC, &start);
-		for (j = 0; j < NR_TESTS_PER_SIZE; j++) {
-			userspace_liteapi_rdma_write(test_key, buf, testsize[i], 0, password);
-		}
-		clock_gettime(CLOCK_MONOTONIC, &end);
-		diff_ns = timespec_diff_ns(end, start);
-
-		rps = (double)NR_TESTS_PER_SIZE / ((double)diff_ns / (double)NSEC_PER_SEC);
-		printf("  CPU%2d  size = %#10x (%10d) avg_time = %15ld ns RPS: %15lf\n",
-			sched_getcpu(),
-			testsize[i], testsize[i], diff_ns/NR_TESTS_PER_SIZE,
-			rps);
+		printf(" \033[31mTest RDMA Write (avg of # %d run)\033[0m\n",
+	       		NR_TESTS_PER_SIZE);
 	}
 
-	printf(" \033[31mTest RDMA Sync Read (avg of # %d run)\033[0m\n", NR_TESTS_PER_SIZE);
 	for (i = 0; i < ARRAY_SIZE(testsize); i++) {
 		struct timespec start, end;
 		long diff_ns;
@@ -107,19 +139,38 @@ static void rdma_write_read()
 
 		clock_gettime(CLOCK_MONOTONIC, &start);
 		for (j = 0; j < NR_TESTS_PER_SIZE; j++) {
-			userspace_liteapi_rdma_read(test_key, buf, testsize[i], 0, password);
+			userspace_liteapi_rdma_write(test_key, buf, testsize[i],
+						     0, password);
 		}
 		clock_gettime(CLOCK_MONOTONIC, &end);
 		diff_ns = timespec_diff_ns(end, start);
 
-		rps = (double)NR_TESTS_PER_SIZE / ((double)diff_ns / (double)NSEC_PER_SEC);
-		printf("  CPU%2d  size = %#10x (%10d) avg_time = %15ld ns RPS: %15lf\n",
-			sched_getcpu(),
-			testsize[i], testsize[i], diff_ns/NR_TESTS_PER_SIZE,
-			rps);
+		rps =
+		    (double)NR_TESTS_PER_SIZE / ((double)diff_ns /
+						 (double)NSEC_PER_SEC);
+
+		per_rps[info->tid] = rps;
+
+		/* Sync among threads */
+		if (pthread_barrier_wait(&thread_barrier) ==
+		    PTHREAD_BARRIER_SERIAL_THREAD) {
+			print_rps(i, "write");
+		}
+
+#if 0
+		printf
+		    ("  [tid %d] CPU%2d  size = %#10x (%10d) avg_time = %15ld ns RPS: %15lf\n",
+		     info->tid, sched_getcpu(), testsize[i], testsize[i],
+		     diff_ns / NR_TESTS_PER_SIZE, rps);
+#endif
 	}
 
-	printf(" \033[31mTest RDMA Async Read (avg of # %d run)\033[0m\n", NR_TESTS_PER_SIZE);
+	if (pthread_barrier_wait(&thread_barrier) ==
+	    PTHREAD_BARRIER_SERIAL_THREAD) {
+		printf(" \033[31mTest RDMA Sync Read (avg of # %d run)\033[0m\n",
+		       NR_TESTS_PER_SIZE);
+	}
+
 	for (i = 0; i < ARRAY_SIZE(testsize); i++) {
 		struct timespec start, end;
 		long diff_ns;
@@ -127,47 +178,130 @@ static void rdma_write_read()
 
 		clock_gettime(CLOCK_MONOTONIC, &start);
 		for (j = 0; j < NR_TESTS_PER_SIZE; j++) {
-			async_rdma_read(test_key, buf, testsize[i], 0, &poll[i]);
+			userspace_liteapi_rdma_read(test_key, buf, testsize[i],
+						    0, password);
 		}
 		clock_gettime(CLOCK_MONOTONIC, &end);
 		diff_ns = timespec_diff_ns(end, start);
 
-		rps = (double)NR_TESTS_PER_SIZE / ((double)diff_ns / (double)NSEC_PER_SEC);
-		printf("  CPU%2d  size = %#10x (%10d) avg_time = %15ld ns RPS: %15lf\n",
-			sched_getcpu(),
-			testsize[i], testsize[i], diff_ns/NR_TESTS_PER_SIZE,
-			rps);
+		rps =
+		    (double)NR_TESTS_PER_SIZE / ((double)diff_ns /
+						 (double)NSEC_PER_SEC);
+		per_rps[info->tid] = rps;
+
+		/* Sync among threads */
+		if (pthread_barrier_wait(&thread_barrier) ==
+		    PTHREAD_BARRIER_SERIAL_THREAD) {
+			print_rps(i, "sync_read");
+		}
+
+#if 1
+		printf
+		    ("  CPU%2d  size = %#10x (%10d) avg_time = %15ld ns RPS: %15lf\n",
+		     sched_getcpu(), testsize[i], testsize[i],
+		     diff_ns / NR_TESTS_PER_SIZE, rps);
+#endif
+	}
+
+	if (pthread_barrier_wait(&thread_barrier) ==
+	    PTHREAD_BARRIER_SERIAL_THREAD) {
+		printf(" \033[31mTest RDMA Async Read (avg of # %d run)\033[0m\n",
+		       NR_TESTS_PER_SIZE);
+	}
+
+	for (i = 0; i < ARRAY_SIZE(testsize); i++) {
+		struct timespec start, end;
+		long diff_ns;
+		double rps;
+
+		clock_gettime(CLOCK_MONOTONIC, &start);
+		for (j = 0; j < NR_TESTS_PER_SIZE; j++) {
+			async_rdma_read(test_key, buf, testsize[i], 0,
+					&poll[i]);
+		}
+		clock_gettime(CLOCK_MONOTONIC, &end);
+		diff_ns = timespec_diff_ns(end, start);
+
+		rps =
+		    (double)NR_TESTS_PER_SIZE / ((double)diff_ns /
+						 (double)NSEC_PER_SEC);
+		per_rps[info->tid] = rps;
+
+		/* Sync among threads */
+		if (pthread_barrier_wait(&thread_barrier) ==
+		    PTHREAD_BARRIER_SERIAL_THREAD) {
+			print_rps(i, "async_read");
+		}
+#if 1
+		printf
+		    ("  CPU%2d  size = %#10x (%10d) avg_time = %15ld ns RPS: %15lf\n",
+		     sched_getcpu(), testsize[i], testsize[i],
+		     diff_ns / NR_TESTS_PER_SIZE, rps);
+#endif
 	}
 }
 
-static int bind_thread(int cpu_id)
-{
-	cpu_set_t cpu_set;
-
-	CPU_ZERO(&cpu_set);
-	CPU_SET(cpu_id, &cpu_set);
-
-	return sched_setaffinity(0, sizeof (cpu_set), &cpu_set);
-}
+static struct option long_options[] = {
+	{ .name = "remote_nid",		.has_arg = 1, .val = 'n' },
+	{ .name = "thread",		.has_arg = 1, .val = 't' },
+};
 
 int main(int argc, char *argv[])
 {
+	int i;
+	struct thread_info *info;
+	pthread_t *threads;
 	int cpu;
 
-        if (argc != 2) {
-		printf("Usage: ./latency remote_node_id\n");
-		return -EINVAL;
+	while (1) {
+		int c;
+
+		c = getopt_long(argc, argv, "t:n:",
+				long_options, NULL);
+
+		if (c == -1)
+			break;
+
+		switch (c) {
+		case 'n':
+			remote_node = strtoul(optarg, NULL, 0);
+			if (remote_node > 16) {
+				return -1;
+			}
+			break;
+		case 't':
+			nr_threads = strtoul(optarg, NULL, 0);
+			if (nr_threads > 56) {
+				printf("Too many threads: %d\n", nr_threads);
+				return -1;
+			}
+			break;
+		default:
+			return -1;
+		};
 	}
 
+	if (remote_node < 1)
+		return -1;
+
+	printf("remote_nid: %d nr_threads: %d\n", remote_node, nr_threads);
+
 	pg_size = sysconf(_SC_PAGESIZE);
-	remote_node = atoi(argv[1]);
 
-	//bind_thread(42);
-	bind_thread(55);
-	printf("Start testing on CPU%2d\n", sched_getcpu());
+	threads = malloc(sizeof(*threads) * nr_threads);
+	per_rps = malloc(sizeof(*per_rps) * nr_threads);
+	info = malloc(sizeof(*info) * nr_threads);
+	memset(info, 0, sizeof(*info) * nr_threads);
 
-	rdma_write_read();
+	pthread_barrier_init(&thread_barrier, NULL, nr_threads);
 
-	printf("Finish testing on CPU%2d\n", sched_getcpu());
+	for (i = 0; i < nr_threads; i++) {
+		info[i].tid = i;
+		info[i].remote_nid = remote_node;
+               	pthread_create(&threads[i], NULL, rdma_write_read, info + i);
+	}
+	for (i = 0; i < nr_threads; i++)
+		pthread_join(threads[i], NULL);
+
 	return 0;
 }
